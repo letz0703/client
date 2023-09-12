f1d6e6cf533f3c51b4d16572d93cd78376723035
ManI <letz0703@gmail.com>
2023-09-13 03:24:33 +0900

    children for posts

--- src/router.jsx
+++ src/router.jsx
@@ -3,6 +3,7 @@ import {UserList} from "./pages/UserList"
 import {PostList} from "./pages/PostList"
 import {TodoList} from "./pages/TodoList"
 import {RootLayout} from "./layouts/RootLayout"
+import axios from "axios"
 
 export const router = createBrowserRouter([
   {
@@ -11,8 +12,24 @@ export const router = createBrowserRouter([
     children: [
       {index: true, element: <Navigate to="posts" />},
       {path: "users", element: <UserList />},
-      {path: "posts", element: <PostList />},
-      //todo: children
+      {
+        path: "posts",
+        children: [
+          {
+            index: true,
+            loader: ({request: {signal}}) => {
+              return axios
+                .get(`http://jsonplaceholder.typicode.com/posts`, {
+                  signal
+                })
+                .then(res => res.data)
+            },
+            element: <PostList />
+          },
+          {path: ":postId", element: <h1>hi</h1>}
+        ]
+      },
+      //{path: "posts", element: <PostList />},
       {path: "todos", element: <TodoList />}
     ]
   }
