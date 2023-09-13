2d3789e3a3482ecabff897fbec734d308446c45d
ManI <letz0703@gmail.com>
2023-09-14 05:08:25 +0900

    create post page

--- src/pages/PostList.jsx
+++ src/pages/PostList.jsx
@@ -1,6 +1,29 @@
 import axios from "axios"
+import {Link, useLoaderData} from "react-router-dom"
 function PostList() {
-  return <h1>PostList</h1>
+  const posts = useLoaderData()
+  return (
+    <>
+      <div className="container">
+        <h1 className="page-title">Posts</h1>
+        <div className="card-grid">
+          {posts.map(post => (
+            <div className="card">
+              <div className="card-header">{post.title}</div>
+              <div className="card-body">
+                <div className="card-preview-text">{post.body}</div>
+              </div>
+              <div className="card-footer">
+                <Link className="btn" to="post.html">
+                  View
+                </Link>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
+  )
 }
 
 const loader = async ({request: {signal}}) => {
