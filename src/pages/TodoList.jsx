6f2b6871aaeb74b1afd44445c117146f244b16d0
ManI <letz0703@gmail.com>
2023-09-19 04:05:50 +0900

    todo.completed?"hidden":""

--- src/pages/TodoList.jsx
+++ src/pages/TodoList.jsx
@@ -2,7 +2,21 @@ import {useLoaderData} from "react-router-dom"
 import {getTodos} from "../api/todos"
 function TodoList() {
   const todos = useLoaderData()
-  return <h1>TodoList-{todos.length}</h1>
+  return (
+    <>
+      <div class="container">
+        <h1 class="page-title">Todos</h1>
+        <ul>
+          {todos.map(todo => (
+            <li key={todo.id} className={todo.completed ? "hidden" : ""}>
+              <input type="checkbox" checked={todo.completed} />
+              {todo.title}
+            </li>
+          ))}
+        </ul>
+      </div>
+    </>
+  )
 }
 
 export const loader = ({request: {signal}}) => {
