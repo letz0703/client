d42c1185f5b2252a21b9059054c96ed6af590719
ManI <letz0703@gmail.com>
2023-09-20 21:29:13 +0900

    loader params

--- src/pages/Post.jsx
+++ src/pages/Post.jsx
@@ -1,15 +1,6 @@
 import {useLoaderData} from "react-router-dom"
 import {getPost} from "../api/posts"
 
-function Post() {
-  return <h1>hi</h1>
-}
-
-const loader = ({request: {signal}}) => {
-  return <h1>hi</h1>
-  //return axios.get(`http://localhost:3000/`, {signal}).then(res => res.data)
-}
-
 export const postRoute = {
   loader,
   element: <Post />
