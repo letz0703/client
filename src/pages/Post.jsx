aec58ec9cc0809df93fcc3c0fa7e93bd055121f7
ManI <letz0703@gmail.com>
2023-09-20 23:53:51 +0900

    Post.jsx postRoute

--- src/pages/Post.jsx
+++ src/pages/Post.jsx
@@ -1,6 +1,15 @@
 import {useLoaderData} from "react-router-dom"
 import {getPost} from "../api/posts"
 
+function Post() {
+  const post = useLoaderData()
+  return post.title
+}
+
+const loader = ({request: {signal}, params}) => {
+  return getPost(params.postId, {signal})
+}
+
 export const postRoute = {
   loader,
   element: <Post />
