3b056b26457ce9a7998421e9074dfc35023009e5
ManI <letz0703@gmail.com>
2023-09-22 01:05:33 +0900

    UserList.jsx

--- src/pages/PostList.jsx
+++ src/pages/PostList.jsx
@@ -1,4 +1,3 @@
-import axios from "axios"
 import {getPosts} from "../api/posts"
 import {Link, useLoaderData} from "react-router-dom"
 function PostList() {
--- src/pages/UserList.jsx
+++ src/pages/UserList.jsx
@@ -1,40 +1,37 @@
-import {useLoaderData} from "react-router-dom"
+import {Link, useLoaderData} from "react-router-dom"
 import {getUsers} from "../api/users.jsx"
 
 function UserList() {
-  const user = useLoaderData()
-  return "hi"
-  //const users = useLoaderData()
-  //return (
-  //  <>
-  //    <div className="container">
-  //      <h1 className="page-title">Users</h1>
-  //      <div className="card">
-  //        {users.map(user => [
-  //          <>
-  //            <div className="card-header">{user.name}</div>
-  //            <div className="card-body">
-  //              <div>{user.company.name}</div>
-  //              <div>{user.website}</div>
-  //              <div>{user.email}</div>
-  //            </div>
-  //            <div className="card-footer">
-  //              <Link className="btn" to={user.id.toString()}>
-  //                {/*<Link className="btn" to={`/users/${user.id}`}>*/}
-  //                View
-  //              </Link>
-  //            </div>
-  //          </>
-  //        ])}
-  //      </div>
-  //    </div>
-  //  </>
-  //)
+  const users = useLoaderData()
+  return (
+    <>
+      <div className="container">
+        <h1 className="page-title">Users</h1>
+        <div className="card">
+          {users.map(user => [
+            <>
+              <div className="card-header">{user.name}</div>
+              <div className="card-body">
+                <div>{user.company.name}</div>
+                <div>{user.website}</div>
+                <div>{user.email}</div>
+              </div>
+              <div className="card-footer">
+                <Link className="btn" to={user.id.toString()}>
+                  {/*<Link className="btn" to={`/users/${user.id}`}>*/}
+                  View
+                </Link>
+              </div>
+            </>
+          ])}
+        </div>
+      </div>
+    </>
+  )
 }
 
-const loader = ({request: {signal}}, params) => {
-  //return getUsers(params.id, {signal})
-  return "hi"
+const loader = ({request: {signal}}) => {
+  return getUsers({signal})
 }
 
 export const UserListRoute = {
